94629c671e355faef6c3e6c821e412f5e16af838
盧重伍 <python41141218@gmail.com>
2023-06-15 00:55:09 +0800

    6.15改

--- db.py
+++ db.py
@@ -6,7 +6,10 @@ print ("Opened database successfully")
 #conn.execute('CREATE TABLE Users (id primarykey auto_increment, name nvarchar(20), account nvarchar(40), password nvarchar(40))')
 #print ("Table created successfully")
 
-conn.execute('CREATE TABLE Pictures (id integer primary key autoincrement, p_name nvarchar(20), p_order integer)')
+#conn.execute('CREATE TABLE Pictures (id integer primary key autoincrement, p_name nvarchar(20), p_order integer)')
+#print ("Table created successfully")
+
+conn.execute('CREATE TABLE Suggestion (id integer primary key autoincrement, s_name nvarchar(20), s_sug integer)')
 print ("Table created successfully")
 
 conn.close()
